f0b5c5b1ec89d1f3b5b6e37b8a489da00f4dc82b
GugaDavi <gguedes.tech@gmail.com>
2019-05-08 11:42:55 -0300

    Challenge 27

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -1,21 +1,74 @@
-/*
-Aproveitando a lib DOM que fizemos na semana anterior, crie agora para ela
-métodos semelhantes aos que existem no array, mas que sirvam para os
-elementos do DOM selecionados.
-Crie os seguintes métodos:
-- forEach, map, filter, reduce, reduceRight, every e some.
+(function (win, doc) {
+  'use strict';
+  /*
+  Aproveitando a lib DOM que fizemos na semana anterior, crie agora para ela
+  métodos semelhantes aos que existem no array, mas que sirvam para os
+  elementos do DOM selecionados.
+  Crie os seguintes métodos:
+  - forEach, map, filter, reduce, reduceRight, every e some.
 
-Crie também métodos que verificam o tipo do objeto passado por parâmetro.
-Esses métodos não precisam depender de criar um novo elmento do DOM, podem
-ser métodos estáticos.
+  Crie também métodos que verificam o tipo do objeto passado por parâmetro.
+  Esses métodos não precisam depender de criar um novo elmento do DOM, podem
+  ser métodos estáticos.
 
-Métodos estáticos não obrigam o uso do `new`, podendo ser usados diretamente
-no objeto, como nos exemplos abaixo:
-DOM.isArray([1, 2, 3]); // true
-DOM.isFunction(function() {}); // true
-DOM.isNumber('numero'); // false
+  Métodos estáticos não obrigam o uso do `new`, podendo ser usados diretamente
+  no objeto, como nos exemplos abaixo:
+  DOM.isArray([1, 2, 3]); // true
+  DOM.isFunction(function() {}); // true
+  DOM.isNumber('numero'); // false
 
-Crie os seguintes métodos para verificação de tipo:
-- isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
-O método isNull deve retornar `true` se o valor for null ou undefined.
-*/
+  Crie os seguintes métodos para verificação de tipo:
+  - isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
+  O método isNull deve retornar `true` se o valor for null ou undefined.
+  */
+
+  function DOM() {
+
+    this.on = function on( event, callBackFuction ) {
+      addEventListener( event, callBackFuction, false );
+    };
+    this.off = function off( event, callBackFuction ) {
+      removeEventListener( event, callBackFuction );
+    };
+    this.get = function get() {
+      return tag;
+    };
+  }
+
+  DOM.prototype.isArray = function isArray(obj) {
+    return Object.prototype.toString.call(obj) === '[object Array]';
+  };
+
+  DOM.prototype.isObject = function isObject(obj) {
+    return Object.prototype.toString.call(obj) === '[object Object]';
+  };
+
+  DOM.prototype.isFunction = function isFunction(obj) {
+    return Object.prototype.toString.call(obj) === '[object Function]';
+  };
+
+  DOM.prototype.isNumber = function isNumber(obj) {
+    return Object.prototype.toString.call(obj) === '[object Number]';
+  };
+
+  DOM.prototype.isString = function isString(obj) {
+    return Object.prototype.toString.call(obj) === '[object String]';
+  };
+
+  DOM.prototype.isBoolean = function isBoolean(obj) {
+    return Object.prototype.toString.call(obj) === '[object Boolean]';
+  };
+
+  DOM.prototype.isNull = function isNull(obj) {
+    var result = Object.prototype.toString.call(obj);
+    var nulo = '[object Null]';
+    var unde = '[object Undefined]';
+    return result === nulo || result === unde ? true : false;
+  };
+
+  var dom = new DOM();
+
+  console.log(dom.isNull(1));
+
+
+})(window, document)
